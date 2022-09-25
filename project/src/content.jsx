26add8e33fac79e7228056e97905b621c330f329
Spagett41 <erik.hedlund@elev.ga.ntig.se>
2022-09-25 20:54:58 +0200

    Fixed timeline and updated content positions

--- project/src/content.jsx
+++ project/src/content.jsx
@@ -18,28 +18,30 @@ function Content() {
                         <p>some image</p>
                     </div>
                 </div>
-                <div>
+                <div id='aboutme' className='text-lg'>
                     <h2 className='text-4xl mt-12 underline underline-offset-4 decoration-neutral-400'>About me</h2>
-                    <p className='text-neutral-100 text-justify mt-6'>&nbsp; I am Erik Hedlund, a software development student from Sweden. I am currently studying a fourth year of high school where I get to work in projects in groups with other students while using common working practices. <br/> <br/> I have been programming for over three years and I have primarly worked with python and web development using html, css and javascript. </p>
+                    <p className='text-neutral-100 text-justify mt-6'>&nbsp; I am Erik Hedlund, a software development student from Sweden. I am currently studying a fourth year of high school where I get to work in projects in groups with other students while using common working practices. <br/> <br/> I have been programming for over three years and I have primarly worked with python and web development using html, css and javascript. <br/> <br/> </p>
+                    <p><span className='font-bold'>2019-2022</span> Studied information technology</p>
+                    <p><span className='font-bold'>2022-present</span> Studying as a high school engineer</p>
                 </div>
-                <div>
+                <div id='projects'>
                     <h2 className='text-4xl mt-12 underline underline-offset-4 decoration-neutral-400'>Projects</h2>
-                    <div className='flex items-center w-full h-32 mt-6'>
-                        <div className='w-1/2'>
+                    <div className='flex items-center w-full h-40 mt-12'>
+                        <div className='flex flex-col w-3/5 h-full'>
                             <p className='text-2xl'>Frisör Saxé</p>
                             <p>Website for a hairdresser saloon.</p>
+                            <a href={'https://ntig-uppsala.github.io/Frisor-Saxe/personal.html'} className='text-2xl text-black w-64 flex justify-center mt-4 mb-12 p-2 bg-orange-200 rounded-lg hover:bg-orange-300 duration-150'>Preview project</a>
                         </div>
-                        <img src='/images/frisorsaxe.png' alt='image of project for Frisör Saxé' className='w-1/2 h-32 object-cover rounded-[1rem]'></img>
+                        <img src='/images/frisorsaxe.png' alt='image of project for Frisör Saxé' className='w-2/5 h-40 object-cover rounded-[1rem]'></img>
                     </div>
-                    <a href={'https://ntig-uppsala.github.io/Frisor-Saxe/personal.html'} className='text-2xl w-full flex justify-center mt-4 hover:underline underline-offset-2 mb-12'>Preview project</a>
-                    <div className='flex items-center w-full h-32 mt-6'>
-                        <div className='w-1/2'>
+                    <div id='contact' className='flex items-center w-full h-40 mt-12'>
+                        <div className='flex flex-col w-3/5 h-full'>
                             <p className='text-2xl'>Florist Blåklint</p>
                             <p>Website for a flower shop.</p>
+                            <a href={'https://ntig-uppsala.github.io/florist-blaklinten/'} className='text-2xl text-black w-64 flex justify-center mt-4 mb-12 p-2 bg-orange-200 rounded-lg hover:bg-orange-300 duration-150'>Preview project</a>
                         </div>
-                        <img src='/images/floristblaklint.png' alt='image of project for Florist Blåklint' className='w-1/2 h-32 object-cover rounded-[1rem]'></img>
+                        <img src='/images/floristblaklint.png' alt='image of project for Florist Blåklint' className='w-2/5 h-40 object-cover rounded-[1rem]'></img>
                     </div>
-                    <a href={'https://ntig-uppsala.github.io/florist-blaklinten/'} className='text-2xl w-full flex justify-center mt-4 hover:underline underline-offset-2 mb-12'>Preview project</a>
                 </div>
                 <div>
                     <h2 className='text-4xl mt-12 underline underline-offset-4 decoration-neutral-400'>Contact</h2>
--- project/src/header.jsx
+++ project/src/header.jsx
@@ -4,14 +4,14 @@ import './index.css'
 function Header() {
 
   return (
-    <nav className='w-full flex justify-center fixed backdrop-blur-sm'>
+    <nav className='w-full flex justify-center fixed backdrop-blur'>
       <div className='h-16 w-[48rem] flex justify-between items-center text-neutral-50'>
         <div className='flex text-lg'>
           <h1 className='font-bold'><a href={'#'}>Erik Hedlund</a></h1>
           <div className='text-neutral-100'>
-            <a href={'#'} className='ml-10 hover:underline'>About me</a>
-            <a href={'#'} className='ml-10 hover:underline'>Projects</a>
-            <a href={'#'} className='ml-10 hover:underline'>Contact</a>
+            <a href={'#aboutme'} className='ml-10 hover:underline'>About me</a>
+            <a href={'#projects'} className='ml-10 hover:underline'>Projects</a>
+            <a href={'#contact'} className='ml-10 hover:underline'>Contact</a>
           </div>
         </div>
         <div className='bg-orange-200 text-black p-4'>
