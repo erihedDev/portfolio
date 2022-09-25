fe5622bd7cd6053cc9891dd3c84a84b2be23a1b5
Spagett41 <erik.hedlund@elev.ga.ntig.se>
2022-09-25 21:37:48 +0200

    Updated information with undertitles and knowledge

--- project/src/content.jsx
+++ project/src/content.jsx
@@ -20,9 +20,13 @@ function Content() {
                 </div>
                 <div id='aboutme' className='text-lg'>
                     <h2 className='text-4xl mt-12 underline underline-offset-4 decoration-neutral-400'>About me</h2>
-                    <p className='text-neutral-100 text-justify mt-6'>&nbsp; I am Erik Hedlund, a software development student from Sweden. I am currently studying a fourth year of high school where I get to work in projects in groups with other students while using common working practices. <br/> <br/> I have been programming for over three years and I have primarly worked with python and web development using html, css and javascript. <br/> <br/> </p>
+                    <h3 className='mt-8 mb-1 text-2xl'>Summary</h3>
+                    <p className='text-neutral-100 text-justify'>&nbsp; I am Erik Hedlund, a software development student from Sweden. I am currently studying a fourth year of high school where I get to work in projects in groups with other students while using common working practices. <br/> <br/> I have been programming for over three years and I have primarly worked with python and web development using html, css and javascript.</p>
+                    <h3 className='mt-8 mb-1 text-2xl'>Timeline</h3>
                     <p><span className='font-bold'>2019-2022</span> Studied information technology</p>
                     <p><span className='font-bold'>2022-present</span> Studying as a high school engineer</p>
+                    <h3 className='mt-8 mb-1 text-2xl'>Knowledge in web development</h3>
+                    <p>HTML, CSS, Javascript, mySQL, Node.js, react, vite, tailwindcss, three.js.</p>
                 </div>
                 <div id='projects'>
                     <h2 className='text-4xl mt-12 underline underline-offset-4 decoration-neutral-400'>Projects</h2>
@@ -32,7 +36,7 @@ function Content() {
                             <p>Website for a hairdresser saloon.</p>
                             <a href={'https://ntig-uppsala.github.io/Frisor-Saxe/personal.html'} className='text-2xl text-black w-64 flex justify-center mt-4 mb-12 p-2 bg-orange-200 rounded-lg hover:bg-orange-300 duration-150'>Preview project</a>
                         </div>
-                        <img src='/images/frisorsaxe.png' alt='image of project for Frisör Saxé' className='w-2/5 h-40 object-cover rounded-[1rem]'></img>
+                        <img src='/images/frisorsaxe.png' alt='image of project for Frisör Saxé' className='w-2/5 h-40 object-cover rounded-lg'></img>
                     </div>
                     <div id='contact' className='flex items-center w-full h-40 mt-12'>
                         <div className='flex flex-col w-3/5 h-full'>
@@ -40,7 +44,7 @@ function Content() {
                             <p>Website for a flower shop.</p>
                             <a href={'https://ntig-uppsala.github.io/florist-blaklinten/'} className='text-2xl text-black w-64 flex justify-center mt-4 mb-12 p-2 bg-orange-200 rounded-lg hover:bg-orange-300 duration-150'>Preview project</a>
                         </div>
-                        <img src='/images/floristblaklint.png' alt='image of project for Florist Blåklint' className='w-2/5 h-40 object-cover rounded-[1rem]'></img>
+                        <img src='/images/floristblaklint.png' alt='image of project for Florist Blåklint' className='w-2/5 h-40 object-cover rounded-lg'></img>
                     </div>
                 </div>
                 <div>
