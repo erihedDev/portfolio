c8b78c1afbc4785f06c7051f6d5777f8a4689c50
Spagett41 <erik.hedlund@elev.ga.ntig.se>
2022-09-26 20:11:43 +0200

    updated to standard threejs

--- project/src/content.jsx
+++ project/src/content.jsx
@@ -9,21 +9,11 @@ import Epitome from './content_components/epitome'
 
 import Model from './model'
 
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-
 function Content() {
     return (
         <div className='flex justify-center w-full'>
             <div className='w-[32rem] h-full mt-20 text-neutral-50'>
-                <div className='h-96'>
-                    <Canvas className='z-0'>
-                        <OrbitControls enableZoom={false}/>
-                        <ambientLight intensity={0.5}></ambientLight>
-                        <directionalLight position={[-2, 5, 2]} intensity={1}></directionalLight>
-                        <Model/>
-                    </Canvas>
-                </div>
+                <Model/>
                 <Introduction/>
                 <Aboutme/>
                 <Projects/>
--- project/src/model.jsx
+++ project/src/model.jsx
@@ -1,20 +1,80 @@
 import React, { useEffect } from 'react'
 import './index.css'
 
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import $ from 'jquery'
+
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 function Model() {
-    const gltf = useLoader(GLTFLoader, './voxel_lucky_cat/scene.gltf')
-    const pi = Math.PI;
     useEffect(() => {
-        gltf.scene.scale.set(2, 2, 2)
-        gltf.scene.rotateX((pi / 180) * 15)
-        
+        const scene = new THREE.Scene()
+
+        const width = $("#model").parent().width()
+        const height = $("#model").parent().height()
+
+        const camera = new THREE.PerspectiveCamera(
+            50,
+            width / height,
+            1,
+            1000,
+        )
+
+        camera.position.z = 96
+
+        const canvas = document.getElementById('model')
+        const renderer = new THREE.WebGLRenderer({
+            canvas,
+            antialias: true,
+        })
+
+        renderer.setSize(width, height)
+        renderer.setClearColor( 0xffffff, 0);
+
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+        ambientLight.castShadow = true
+        scene.add(ambientLight)
+
+        const spotLight = new THREE.SpotLight(0xffffff, 1)
+        spotLight.castShadow = true
+        spotLight.position.set(0, 64, 32)
+        scene.add(spotLight)
+
+        const loader = new GLTFLoader()
+
+        let model
+
+        loader.load('/models/voxelcat.glb', function (gltf) {
+            gltf.scene.scale.set( 30, 30, 30 )
+            gltf.scene.rotation.x = (Math.PI / 180) * 15
+            model = gltf
+            scene.add(gltf.scene)
+        }, undefined, function (error) {
+            console.error(error)
+        }
+        )
+
+        const controls = new OrbitControls( camera, renderer.domElement );
+        controls.enableZoom = false;
+
+        const animate = () => {
+            if (model) {
+                model.scene.rotation.y += 0.005
+            }
+
+            controls.update()
+            renderer.render(scene, camera)
+            window.requestAnimationFrame(animate)
+        }
+        animate()
+
     })
 
     return (
-        <primitive object={gltf.scene}/>
+        <div className='w-full h-96'>
+            <canvas id='model'/>
+        </div>
     )
 }
 
