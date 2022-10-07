439145de27b063954147872c8040937f07260265
erihedDev <erik.hedlund@elev.ga.ntig.se>
2022-10-07 13:41:35 +0200

    updated dist

--- project/src/content_components/projects.jsx
+++ project/src/content_components/projects.jsx
@@ -12,7 +12,7 @@ function Projects() {
                     <p>Website for a hairdresser saloon.</p>
                     <a href={'https://ntig-uppsala.github.io/Frisor-Saxe/'} className='text-2xl text-center max-w-fit flex justify-center mt-4 mb-12 p-2 bg-orange-200 rounded-lg hover:bg-orange-300 duration-150 text-neutral-900'>Preview project</a>
                 </div>
-                <img src='project/src/assets/images/frisorsaxe.png' alt='image of project for Frisör Saxé' className='w-64 sm:w-2/5 h-40 object-cover rounded-lg'></img>
+                <img src='images/frisorsaxe.png' alt='image of project for Frisör Saxé' className='w-64 sm:w-2/5 h-40 object-cover rounded-lg'></img>
             </div>
 
             <div className='flex flex-col-reverse sm:flex-row items-center w-full h-40 mt-48 sm:mt-12'>
@@ -21,7 +21,7 @@ function Projects() {
                     <p>Website for a flower shop.</p>
                     <a href={'https://ntig-uppsala.github.io/florist-blaklinten/'} className='text-2xl text-center max-w-fit flex justify-center mt-4 mb-12 p-2 bg-orange-200 rounded-lg hover:bg-orange-300 duration-150 text-neutral-900'>Preview project</a>
                 </div>
-                <img src='project/src/assets/images/floristblaklint.png' alt='image of project for Florist Blåklint' className='w-64 sm:w-2/5 h-40 object-cover rounded-lg'></img>
+                <img src='images/floristblaklint.png' alt='image of project for Florist Blåklint' className='w-64 sm:w-2/5 h-40 object-cover rounded-lg'></img>
             </div>
         </div>
     )
