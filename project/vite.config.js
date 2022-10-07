f5c41f5f4ed082e52dc51dc6f754e7b120e4a6e2
erihedDev <erik.hedlund@elev.ga.ntig.se>
2022-10-07 11:43:49 +0200

    Updated base title

--- project/vite.config.js
+++ project/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://<erihedDev>.github.io/<vitejs-portfolio>/',
+  base: 'https://<erihedDev>.github.io/<portfolio>/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
