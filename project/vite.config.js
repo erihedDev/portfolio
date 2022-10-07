30420a5353af45d9c87cdd4f4baf33b1ce5fa215
erihedDev <erik.hedlund@elev.ga.ntig.se>
2022-10-07 11:42:45 +0200

    Preparation for github pages publishing

--- project/vite.config.js
+++ project/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project/',
-  plugins: [react()]
+  base: 'https://<erihedDev>.github.io/<vitejs-portfolio>/',
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
